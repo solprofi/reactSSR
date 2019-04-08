18dbca969c9df48acedd9496322f45c960e275df
Solohor <roman.solohor@sap.com>
2019-04-08 16:00:23 +0200

    added server boilerplate

---
+++ src/client/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Homeboi
+    </div>  
+  )
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const React = require('react');
+const { renderToString } = require('react-dom/server');
+const Home = require('./client/components/Home').default;
+
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+  res.send(content);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
